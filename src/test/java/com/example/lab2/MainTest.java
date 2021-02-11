6c740fe6100519b676df934520add07737e0e04d
Alina <al.t.shevchenko@gmail.com>
2021-02-11 19:51:34 +0200

    initial commit

---
+++ src/test/java/com/example/lab2/MainTest.java
@@ -0,0 +1,13 @@
+package com.example.lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
