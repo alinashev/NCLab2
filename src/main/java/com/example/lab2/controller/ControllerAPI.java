cf60938a54d6096d3a2e93f258e113d8f2600516
alina <al.t.shevchenko@gmail.com>
2021-04-12 21:47:40 +0300

    #6: changed annotations

--- src/main/java/com/example/lab2/controller/ControllerAPI.java
+++ src/main/java/com/example/lab2/controller/ControllerAPI.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -22,7 +23,7 @@ import java.util.Calendar;
 import java.util.Locale;
 
 
-@Controller
+@RestController
 public class ControllerAPI {
 
     @Autowired
--- src/main/java/com/example/lab2/controller/StartPageController.java
+++ src/main/java/com/example/lab2/controller/StartPageController.java
@@ -1,10 +1,10 @@
 package com.example.lab2.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class StartPageController {
     @GetMapping("/")
     public String index(Model model){
